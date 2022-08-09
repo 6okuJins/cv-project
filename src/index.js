bb1c675e146bb8ad9b01d94b3856c9bff219de6a
6okuJins <d.ma@mail.utoronto.ca>
2022-08-09 04:29:04 -0400

    Add favicon and change site title

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 import App from './App';
 import './styles';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <title>Make CV</title>
+      <link rel="icon" href="./assets/favicon.ico" />
+    </Helmet>
     <App />
   </React.StrictMode>
 )
